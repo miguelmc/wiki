88e6918b6c1a12269c52b27364131070c334941e
Patricio Beltran <pbelsal@gmail.com>
2013-05-01 13:45:00 -0500

    Init wiki

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wiki::Application.config.secret_token = '4a6b1c682985d2ff2193f85823e7468c71944bbc6533d9e81b4a4da864d79655b6d203a0ab38612aa3e4b5e69c196bc922ed6b1c1870c869cf2311854263b062'
