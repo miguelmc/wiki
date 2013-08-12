9364f28fcf314dc09a4491a0222bbef315fbd85a
PatoBeltran <pbelsal@gmail.com>
2013-08-12 17:42:06 -0500

    seed for creating admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,8 @@
 # encoding: utf-8
+
+user = User.create(username: "admin", password: "admin", password_confirmation: "admin", email: "no-reply@icalialabs.com")
+user.toggle!(:admin)
+
 Category.create(name: "Plug-ins")
 Category.create(name: "Móvil")
 Category.create(name: "UI/UX")
