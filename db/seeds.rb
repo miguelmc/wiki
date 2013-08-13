3dda6330934a87b96f0e430a442d2da2f593379b
Abraham Kuri <kurenn@icalialabs.com>
2013-08-13 17:52:21 -0500

    Change admin email for the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,7 @@
 # encoding: utf-8
 
-user = User.create(username: "admin", password: "admin", password_confirmation: "admin", email: "no-reply@icalialabs.com")
+user = User.create(username: "admin", 
+                   password: "admin", 
+                   password_confirmation: "admin", 
+                   email: "admin@icalialabs.com")
 user.toggle!(:admin)
-
-Category.create(name: "Plug-ins")
-Category.create(name: "Móvil")
-Category.create(name: "UI/UX")
-Category.create(name: "Front-end")
-Category.create(name: "Back-end")
-Category.create(name: "Workflow")
-Category.create(name: "Otros")
